10061d5bf4b277b47fb1cfeffa3de0ee602441f7
anoop5.sharma <Anoop Sharma>
2018-12-04 07:54:52 +0530

    dependecy jar

--- src/main/java/ExecutorServicePackage/StockExecutor.java
+++ src/main/java/ExecutorServicePackage/StockExecutor.java
@@ -6,23 +6,52 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.Assert;
 
+import javax.net.ssl.*;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.text.ParseException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class StockExecutor {
+    public static void enableSSLSocket() throws KeyManagementException, NoSuchAlgorithmException {
+        HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
 
-    public static void main(String[] args) {
+        SSLContext context = SSLContext.getInstance("TLS");
+        context.init(null, new X509TrustManager[]{new X509TrustManager() {
+            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            }
+
+            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            }
+
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+        }}, new SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
+    }
+    public static void main(String[] args) throws NoSuchAlgorithmException, KeyManagementException {
+
+enableSSLSocket();
+
         ExecutorService executorService= Executors.newFixedThreadPool(2);
         executorService.execute(new Runnable() {
             @Override
             public void run() {
                 for (int i = 0; i <100000000; i++) {
                     try {
-                        System.out.println("Executed BNOCData Count: "+i);
                         Thread.sleep(30000);
                         new NSEBNDController().execute(0);
+                        System.out.println("Executed BNOCData Count: "+i);
                     } catch (InterruptedException | IOException | ParseException e) {
                         e.printStackTrace();
                     }
@@ -37,10 +66,10 @@ public class StockExecutor {
             public void run() {
                 for (int i = 0; i <10000000 ; i++) {
                     System.out.println("Task 2 " + i);
-                    System.out.println("Executed IncOI Inc Pri: Count "+i);
                     try {
                         Thread.sleep(100000);
                         new DBController().execute(0);
+                        System.out.println("Executed IncOI Inc Pri: Count "+i);
                     } catch (InterruptedException | IOException | ParseException e) {
                         e.printStackTrace();
                     }
--- src/main/java/IndexNSELive/NSEBankNiftyFetcher.java
+++ src/main/java/IndexNSELive/NSEBankNiftyFetcher.java
@@ -31,7 +31,8 @@ public class NSEBankNiftyFetcher {
     }
     public Map<String,Map<String, String>> getMappedData() throws IOException, ParseException {
         Document doc;
-        doc = Jsoup.connect("https://www.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?symbolCode=-9999&symbol=BANKNIFTY&symbol=BANKNIFTY&instrument=OPTIDX&date=-&segmentLink=17&segmentLink=17").get();
+        doc = Jsoup.connect("http://www.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?symbolCode=-9999&symbol=BANKNIFTY&symbol=BANKNIFTY&instrument=OPTIDX&date=-&segmentLink=17&segmentLink=17")
+                .timeout(60000).validateTLSCertificates(false).get();
 //        doc = Jsoup.connect("https://www.moneycontrol.com/stocks/fno/marketstats/futures/oi_inc_p_inc/homebody.php?opttopic=allfut&optinst=allfut&sel_mth=1&sort_order=1").get();
 //        doc = Jsoup.connect("https://www.moneycontrol.com/stocks/fno/marketstats/futures/oi_dec_p_dec/homebody.php?opttopic=allfut&optinst=allfut&sel_mth=1&sort_order=0").get();
 //        doc = Jsoup.connect("https://www.moneycontrol.com/stocks/fno/marketstats/futures/oi_dec_p_dec/homebody.php?opttopic=allfut&optinst=allfut&sel_mth=1&sort_order=0").get();
